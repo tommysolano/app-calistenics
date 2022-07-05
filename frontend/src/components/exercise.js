7960a6e19441cba8e8c413194d2462cc3e06710a
tommy solano <tommysolano18@hotmail.com>
2022-07-05 02:48:19 -0500

    añadimos las cambios al json

--- frontend/src/components/exercise.js
+++ frontend/src/components/exercise.js
@@ -1,13 +1,47 @@
-import React from "react"
+import React, { useState, useEffect, useContext } from "react"
+import { authContext } from "../context/authContext"
 import { useParams } from 'react-router-dom'
+import Axios from 'axios'
 import "../public/css/exercise/exercise.css"
 
 
 function Exercise() {
 
-    let { exercise } = useParams();
+    let { exercisesListTitleParams } = useParams(); // obtengo el parametro de la categoria del ejercicio
+    let { exercise } = useParams(); // obtengo el parametro del ejercicio
 
-    console.log(exercise)
+
+    const [exerciseId, setExerciseId] = useState({});
+    const { auth } = useContext(authContext)
+
+    let dataExercise = []
+
+
+    // obtengo la data de los ejercicios
+    useEffect(() => {
+        Axios({
+            method: "GET",
+            withCredentials: true,
+            url: "http://localhost:5000/api/exercises",
+            headers: {
+                "Content-Type": "application/json",
+                "x-access-token": auth.token
+            }
+        }).then((res) => {
+            setExerciseId(res.data.ejercicios[0])
+        })
+            .catch((err) => console.log(err))
+
+    }, [])
+
+    
+    // pregunto si exerciseId[exercisesListTitleParams] es distinto de undefined en caso de ser verdad lo tranformo en un array dentro de la variable dataExercise
+    if (exerciseId[exercisesListTitleParams] !== undefined) {
+        dataExercise = Object.entries(exerciseId[exercisesListTitleParams][exercise])
+    }
+
+    console.log(dataExercise)
+
 
     return (
         <div className="container_exercise_id">
